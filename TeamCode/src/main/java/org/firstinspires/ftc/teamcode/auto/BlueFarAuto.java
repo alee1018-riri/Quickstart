a39cab9d903968736efc8c50064172c0dc26f30d
alee1018-riri <arielee99@gmail.com>
2026-04-07 19:01:53 -0700

    Initial framework for teleop and auto
    Created initial framework for teleop and auto and learned how to implement OpModes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueFarAuto.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+//Auto.java class
+@Autonomous (name = "Blue Far")
+public class BlueFarAuto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        //one time code here
+        //connect motors, servos, sensors, etc.
+
+
+
+        //Looping code here
+
+        waitForStart();
+
+        while (opModeIsActive()){
+
+
+        }
+
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DecodeRedTeleop.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp (name = "DECODE RED")
+public class DecodeRedTeleop extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Initialization", "successful");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+
+
+
+
+
+
+    }
+}
